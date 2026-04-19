e8ad6173348b336f44e490a6ebc2b0bef3842a56
AKefi <ayoubkefi1@gmail.com>
2026-04-19 21:36:52 +0200

    fixed  creating station method( handles robot name lookups  seperately )

--- api/src/stations/services.py
+++ api/src/stations/services.py
@@ -1,7 +1,11 @@
+from fastapi import HTTPException
+
 from sqlalchemy.orm import Session
 
 from stations.models import Station
 from stations.schemas import StationCreate, StationRead
+from robots.models import Robot
+
 
 class StationService :
     
@@ -9,7 +13,15 @@ class StationService :
         self.db = db
 
     def create_station(self,data:StationCreate) -> StationRead : 
-        station = Station(**data.model_dump()) 
+        station_data = data.model_dump(exclude = {"robot_name"})
+        station = Station(**station_data)
+        if data.robot_name :
+            robot = self.db.query(Robot).filter(Robot.name == data.robot_name).first()
+            if not robot : 
+                raise HTTPException(status_code = 404, detail = "robot not found ")
+            if robot.station is not None :
+                raise HTTPException(status_code = 400, detail = "Robot assigned to another station")
+            station.robot = robot  
         self.db.add(station)
         self.db.commit()
         self.db.refresh(station)
