37d1aecc9d72df62b577dcabf72b9696ef55f970
AKefi <ayoubkefi1@gmail.com>
2026-04-21 00:14:56 +0200

    fix: changed from camelcase to snake_case  naming for collection item service methods

--- api/src/collection_items/routers.py
+++ api/src/collection_items/routers.py
@@ -10,26 +10,26 @@ router = APIRouter(prefix="/collection_items", tags=["collection_items"])
 @router.post("/",response_model=CollectionItemRead)
 def create_collection_item(data: CollectionItemCreate,db: Session = Depends(get_db) ):
     service = CollectionItemService(db)
-    return service.create_collectionItem(data)
+    return service.create_collection_item(data)
 
 @router.get("/", response_model= list[CollectionItemRead])
 def list_collection_items(db:Session = Depends(get_db)):
     service = CollectionItemService(db)
-    return service.list_collectionItems()
+    return service.list_collection_items()
 
 @router.get("/{collection_item_name}", response_model=CollectionItemRead)
 def get_collection_item(collection_item_name: str, db: Session = Depends(get_db)):
     service = CollectionItemService(db)
-    return service.get_collectionItem(collection_item_name)
+    return service.get_collection_item(collection_item_name)
 
 
 @router.patch("/{collection_item_name}", response_model = CollectionItemRead)
 def update_collection_item(collection_item_name : str, data: CollectionItemUpdate, db: Session = Depends(get_db)):
     service =  CollectionItemService(db)
-    return service.update_collectionItem(collection_item_name,data)
+    return service.update_collection_item(collection_item_name,data)
 
 
 @router.delete("/{collection_item_name}", status_code=204)
 def delete_sation(collection_item_name : str, db: Session = Depends(get_db)):
     service = CollectionItemService(db)
-    return service.delete_collectionItem(collection_item_name)
+    return service.delete_collection_item(collection_item_name)
--- api/src/collection_items/services.py
+++ api/src/collection_items/services.py
@@ -10,24 +10,24 @@ class CollectionItemService :
     def __init__(self, db:Session):
         self.db  = db
     
-    def create_collectionItem(self, data:CollectionItemCreate) -> CollectionItemRead :
+    def create_collection_item(self, data:CollectionItemCreate) -> CollectionItemRead :
         collection_item = CollectionItem(**data.model_dump())
         self.db.add(collection_item)
         self.db.commit()
         self.db.refresh(collection_item)
         return collection_item
 
-    def list_collectionItems(self) -> list[CollectionItem] :
+    def list_collection_items(self) -> list[CollectionItem] :
         collection_items = self.db.query(CollectionItem).all()
         return collection_items
     
-    def get_collectionItem(self, collection_item_name: str) -> CollectionItemRead:
+    def get_collection_item(self, collection_item_name: str) -> CollectionItemRead:
         collection_item = self.db.query(CollectionItem).filter(CollectionItem.name == collection_item_name).first()
         if collection_item is None:
             raise_not_found("Collection item",collection_item_name) 
         return collection_item
 
-    def update_collectionItem(self, collection_item_name : str, data: CollectionItemUpdate) -> CollectionItemRead : 
+    def update_collection_item(self, collection_item_name : str, data: CollectionItemUpdate) -> CollectionItemRead : 
         update_data = data.model_dump(exclude_unset = True)
         collectionItem = self.get_collectionItem(collection_item_name)
         for field, value in update_data.items():
@@ -37,7 +37,7 @@ class CollectionItemService :
         return collectionItem 
     
 
-    def delete_collectionItem(self,item_name : str) -> None : 
+    def delete_collection_item(self,item_name : str) -> None : 
         collectionItem = self.get_collectionItem(item_name)
         self.db.delete(collectionItem)
         self.db.commit()
