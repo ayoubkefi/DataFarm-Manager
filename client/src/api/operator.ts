6c9814fd8cee3784365ecc54b53e4b517caf40f5
AKefi <ayoubkefi1@gmail.com>
2026-04-23 00:31:43 +0200

    fix: standardize formatting and add return statements in operator API methods

--- client/src/api/operator.ts
+++ client/src/api/operator.ts
@@ -3,19 +3,20 @@ import client from './client'
 
 
 export const operatorApi = {
-    list : ()  =>  {
-        client.get<Operator[]>('/operators/')
+    list: () => {
+        return client.get<Operator[]>('/operators/')
     },
-    get : (operator_number : number) => {
-        client.get<Operator>(`/operators/${operator_number}`)
+    get: (operator_number: number) => {
+        return client.get<Operator>(`/operators/${operator_number}`)
     },
-    create : (operator : OperatorCreate) => {
-        client.post<Operator>(`/operators/${operator}`)
+    create: (operator: OperatorCreate) => {
+        return client.post<Operator>('/operators/', operator)
     },
-    update : (operator_number : number, data : OperatorUpdate) => {
-        client.patch<Operator>(`/operators/${operator_number}`,data)
+    update: (operator_number: number, data: OperatorUpdate) => {
+        return client.patch<Operator>(`/operators/${operator_number}`, data)
     },
-    delete : (operator_number : number) => {
-        client.delete(`/operators/${operator_number}`)
+    delete: (operator_number: number) => {
+        return client.delete(`/operators/${operator_number}`)
     }
 }
+export default operatorApi
