c8ac3add3a9c4b8d08ef6f078d64e503e09d7e9f
AKefi <ayoubkefi1@gmail.com>
2026-04-26 08:59:58 +0200

    feat: add layout styles and restructure App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { OperatorPage } from "./pages/OperatorPage";
-
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import styles from './App.module.css'
 function App() {
   return (
   <BrowserRouter>
+    <div className={styles.layout}>
+    <Sidebar />
+    <div className={styles.page}>
     <Routes>
       <Route path="/operators" element = {<OperatorPage/>} />
     </Routes>
+    </div>
+    </div>
   </BrowserRouter>
   )
 }
